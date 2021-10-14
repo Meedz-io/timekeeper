02e486848f5e376422e76d986b663a290f0fe45d
Yanis Sadi <dev@sadio.world>
2021-10-14 17:37:04 +0200

    clean project and docker-compose

--- src/index.js
+++
@@ -1,8 +0,0 @@
-require('../env/config.js');
-//const logger = require('./models/logger');
-const { navigateTo } = require('./actions/navigate');
-
-// Exemple of usage
-navigateTo('https://www.linkedin.com')
-    .then(data => console.log(data.body.innerHTML))
-    .catch(err => console.warn(err));
--- src/models/browser.js
+++
@@ -1,8 +0,0 @@
-const puppeteer = require('puppeteer-core');
-
-puppeteer.defaultArgs({
-    headless: process.env.NODE_ENV === 'production', 
-    args: ['--headless']
-});
-
-module.exports = puppeteer;
